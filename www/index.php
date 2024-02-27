de5589f0b0fb8581db22977fc5d435c0bf3f566b
JavaCommons Technologies <javacommons@gmail.com>
2024-02-27 13:04:31 +0900

    Initial commit

---
+++ www/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Example</title>
+    </head>
+    <body>
+
+        <?php
+            echo "Hi, I'm a PHP script!";
+        ?>
+
+        <br />
+
+        <?php
+            echo $_SERVER['HTTP_USER_AGENT'];
+        ?>
+
+     </body>
+</html>
